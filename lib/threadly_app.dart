0fa05147d56bde8fc498e3317ec85693fc5d8acb
Seif Tariq <SeifTariq@gmail.com>
2024-12-19 14:16:24 +0200

    config main widget

---
+++ lib/threadly_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ThreadlyApp extends StatelessWidget {
+  const ThreadlyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
